307212fd4847b170df07f1e8f69099327efd96d0
Rob Leslie <rob@mars.org>
2012-04-15 12:32:42 -0700

    Working libnetfilter_conntrack version
    Use libnetfilter_conntrack to obtain the original destination address
    of NAT-redirected packets. Rewrite the data block to reflect the
    interface-local address relative to the destination in the LOCATION URL,
    then resend the packet to the destination.

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 
 CC	= gcc
-CFLAGS	= -O2 -Wall
+CFLAGS	= -O2 -Wall -Werror
+LDFLAGS	= -lnetfilter_conntrack
 
 upnp-proxy: main.o
-	$(CC) -o $@ $^
+	$(CC) $(LDFLAGS) -o $@ $^
+	sudo setcap CAP_NET_ADMIN+ep $@
--- main.c
+++ main.c
@@ -5,17 +5,23 @@
 # include <netinet/in.h>
 # include <arpa/inet.h>
 
-# include <limits.h>
-# include <linux/netfilter_ipv4.h>
+# include <libnetfilter_conntrack/libnetfilter_conntrack.h>
 
 # include <errno.h>
 # include <stdio.h>
+# include <string.h>
 
 # define PORT 7909
 
 struct context {
-  int in_sockfd;
-  int out_sockfd;
+  int sockfd;
+  struct nfct_handle *cth;
+};
+
+struct answer {
+  int found;
+  const struct sockaddr_in *src;
+  struct sockaddr_in *dst;
 };
 
 static int initialize(struct context *, int);
@@ -26,10 +32,8 @@ int main(int argc, char *argv[])
 {
   struct context context;
 
-# if 0
   if (daemon(0, 0) == -1)
     return 1;
-# endif
 
   if (initialize(&context, PORT) == -1)
     return 2;
@@ -46,19 +50,14 @@ int main(int argc, char *argv[])
 static
 int initialize(struct context *context, int port)
 {
-  int in_fd, out_fd;
+  int fd;
   struct sockaddr_in sin;
+  struct nfct_handle *cth;
 
-  /* create the sockets */
+  /* create the inbound socket */
 
-  in_fd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (in_fd == -1) {
-    perror("socket");
-    return -1;
-  }
-
-  out_fd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (out_fd == -1) {
+  fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (fd == -1) {
     perror("socket");
     return -1;
   }
@@ -69,15 +68,23 @@ int initialize(struct context *context, int port)
   sin.sin_port        = htons(port);
   sin.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-  if (bind(in_fd, (struct sockaddr *) &sin, sizeof(sin)) == -1) {
+  if (bind(fd, (struct sockaddr *) &sin, sizeof(sin)) == -1) {
     perror("bind");
     return -1;
   }
 
-  /* save the sockets in the context */
+  /* get a handle for the netfilter conntrack library */
 
-  context->in_sockfd  = in_fd;
-  context->out_sockfd = out_fd;
+  cth = nfct_open(CONNTRACK, 0);
+  if (cth == 0) {
+    perror("nfct_open");
+    return -1;
+  }
+
+  /* save the socket and handle in the context */
+
+  context->sockfd  = fd;
+  context->cth = cth;
 
   return 0;
 }
@@ -90,28 +97,155 @@ void log_packet(const char *data, int len, const struct sockaddr_in *src)
 }
 
 static
-int find_orig_dst(int sockfd, const struct sockaddr_in *src,
-		  struct sockaddr_in *orig_dst)
+int callback(enum nf_conntrack_msg_type type, struct nf_conntrack *ct,
+	     void *data)
 {
-  struct sockaddr unspec;
-  socklen_t addrlen;
+  struct answer *answer = data;
 
-  unspec.sa_family = AF_UNSPEC;
+  if (nfct_get_attr_u8(ct, ATTR_L3PROTO) == AF_INET &&
+      nfct_get_attr_u8(ct, ATTR_L4PROTO) == IPPROTO_UDP) {
+    const struct sockaddr_in *src = answer->src;
 
-  if (connect(sockfd, (struct sockaddr *) src, sizeof(*src)) == -1) {
+    if (nfct_get_attr_u32(ct, ATTR_REPL_IPV4_DST) == src->sin_addr.s_addr &&
+	nfct_get_attr_u16(ct, ATTR_REPL_PORT_DST) == src->sin_port &&
+	nfct_get_attr_u32(ct, ATTR_REPL_IPV4_SRC) == htonl(INADDR_LOOPBACK) &&
+	nfct_get_attr_u16(ct, ATTR_REPL_PORT_SRC) == htons(PORT)) {
+      struct sockaddr_in *dst = answer->dst;
+# if 0
+      char buffer[1024];
+
+      nfct_snprintf(buffer, sizeof(buffer), ct,
+		    NFCT_T_UNKNOWN, NFCT_O_DEFAULT, NFCT_OF_SHOW_LAYER3);
+      printf(">> %s\n", buffer);
+# endif
+
+      dst->sin_family      = AF_INET;
+      dst->sin_port        = nfct_get_attr_u16(ct, ATTR_PORT_DST);
+      dst->sin_addr.s_addr = nfct_get_attr_u32(ct, ATTR_IPV4_DST);
+
+      answer->found = 1;
+
+# if 0
+      /* this causes libnetfilter_conntrack to fail? */
+      return NFCT_CB_STOP;
+# endif
+    }
+  }
+
+  return NFCT_CB_CONTINUE;
+}
+
+static
+int find_orig_dst(const struct context *context,
+		  const struct sockaddr_in *src, struct sockaddr_in *dst)
+{
+  int result = 0;
+  u_int32_t family = AF_INET;
+  struct answer answer;
+
+  answer.found = 0;
+  answer.src   = src;
+  answer.dst   = dst;
+
+  nfct_callback_register(context->cth, NFCT_T_ALL, callback, &answer);
+  if (nfct_query(context->cth, NFCT_Q_DUMP, &family) == -1) {
+    perror("nfct_query");
+    result = -1;
+  }
+  nfct_callback_unregister(context->cth);
+
+  if (!answer.found)
+    result = -1;
+
+  return result;
+}
+
+# define LOCATION  "LOCATION: http://"
+
+static
+int rewrite_send(const struct context *context, char buffer[], size_t size,
+		 const struct sockaddr_in *dst)
+{
+  char *location;
+  int fd;
+  ssize_t sent;
+
+  /* create the outbound socket */
+
+  fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (fd == -1) {
+    perror("socket");
+    return -1;
+  }
+
+  if (connect(fd, (const struct sockaddr *) dst, sizeof(*dst)) == -1) {
     perror("connect");
+    close(fd);
     return -1;
   }
 
-  addrlen = sizeof(*orig_dst);
-  if (getsockopt(sockfd, SOL_IP, SO_ORIGINAL_DST, orig_dst, &addrlen) == -1) {
-    perror("getsockopt(SO_ORIGINAL_DST)");
-    connect(sockfd, &unspec, sizeof(unspec));
+  /* find the LOCATION header */
+
+  location = strstr(buffer, LOCATION);
+  if (location == 0)
+    fprintf(stderr, "unable to find LOCATION header\n");
+  else {
+    char *location_ip, *location_ip_end;
+
+    location_ip = location + sizeof(LOCATION) - 1;
+    location_ip_end = strchr(location_ip, ':');
+
+    if (location_ip_end == 0)
+      fprintf(stderr, "unable to find LOCATION address end\n");
+    else {
+      struct sockaddr_in src;
+      socklen_t addrlen;
+      char *src_ip;
+      size_t iplen;
+
+      /* determine the address substitute */
+
+      addrlen = sizeof(src);
+      if (getsockname(fd, (struct sockaddr *) &src, &addrlen) == -1) {
+	perror("getsockname");
+	close(fd);
+	return -1;
+      }
+
+      src_ip = inet_ntoa(src.sin_addr);
+      iplen = strlen(src_ip);
+
+      printf("sending reply from %s port %d\n", src_ip, ntohs(src.sin_port));
+
+      memmove(location_ip + iplen, location_ip_end,
+	      size - (location_ip_end - buffer));
+      size = size - (location_ip_end - location_ip) + iplen;
+
+      memcpy(location_ip, src_ip, iplen);
+
+# if 0
+      buffer[size] = 0;
+      printf(">>%s<<\n", buffer);
+# endif
+    }
+  }
+
+  /* send the reply */
+
+  do
+    sent = send(fd, buffer, size, 0);
+  while (sent == -1 && errno == EINTR);
+  if (sent == -1) {
+    perror("send");
+    close(fd);
     return -1;
   }
 
-  if (connect(sockfd, &unspec, sizeof(unspec)) == -1)
-    perror("connect(AF_UNSPEC)");
+  if (sent != size)
+    fprintf(stderr, "sent size doesn't match\n");
+
+  if (close(fd) == -1)
+    perror("close");
 
   return 0;
 }
@@ -119,15 +253,17 @@ int find_orig_dst(int sockfd, const struct sockaddr_in *src,
 static
 int process(struct context *context)
 {
+  printf("receiving packets...\n");
+
   while (1) {
     ssize_t size;
-    char buffer[1500];
-    struct sockaddr_in src, orig_dst;
+    char buffer[1509];
+    struct sockaddr_in src, dst;
     socklen_t addrlen;
 
     do {
       addrlen = sizeof(src);
-      size = recvfrom(context->in_sockfd, buffer, sizeof(buffer), 0,
+      size = recvfrom(context->sockfd, buffer, sizeof(buffer) - 9, 0,
 		      (struct sockaddr *) &src, &addrlen);
     } while (size == -1 && errno == EINTR);
     if (size == -1) {
@@ -141,15 +277,20 @@ int process(struct context *context)
     if (addrlen != sizeof(src))
       return -1;
 
+    buffer[size] = 0;
+
     log_packet(buffer, size, &src);
 
-    if (find_orig_dst(context->in_sockfd, &src, &orig_dst) == -1) {
+    if (find_orig_dst(context, &src, &dst) == -1) {
       fprintf(stderr, "unable to determine original destination\n");
       continue;
     }
 
-    printf("  original destination %s port %d\n",
-	   inet_ntoa(orig_dst.sin_addr), ntohs(orig_dst.sin_port));
+    printf("...original destination %s port %d\n",
+	   inet_ntoa(dst.sin_addr), ntohs(dst.sin_port));
+
+    if (rewrite_send(context, buffer, size, &dst) == -1)
+      fprintf(stderr, "reply failed\n");
   }
 
   return 0;
@@ -158,13 +299,21 @@ int process(struct context *context)
 static
 int finish(struct context *context)
 {
-  /* close the sockets */
+  int result = 0;
 
-  if (close(context->in_sockfd) == -1 ||
-      close(context->out_sockfd) == -1) {
+  /* close the inbound socket */
+
+  if (close(context->sockfd) == -1) {
     perror("close");
-    return -1;
+    result = -1;
   }
 
-  return 0;
+  /* close the netfilter conntrack library */
+
+  if (nfct_close(context->cth) == -1) {
+    perror("nfct_close");
+    result = -1;
+  }
+
+  return result;
 }
